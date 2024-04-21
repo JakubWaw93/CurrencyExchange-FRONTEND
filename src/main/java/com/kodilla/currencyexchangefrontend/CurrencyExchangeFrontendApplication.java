0ee0cb111948135ad76f1be2a44f8423d89a24af
JakubWaw93 <149072115+JakubWaw93@users.noreply.github.com>
2024-04-21 15:55:28 +0200

    init commit

---
+++ src/main/java/com/kodilla/currencyexchangefrontend/CurrencyExchangeFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.currencyexchangefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/currencyexchangefrontend/CurrencyExchangeFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.currencyexchangefrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
